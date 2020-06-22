d841824bab37b0410bf45725e7b9bc2460cde26b
Marius Tolzmann <m@rius.berlin>
2020-06-22 02:50:16 +0200

    Update vpc test to check for idempotency

--- test/vpc_test.go
+++ test/vpc_test.go
@@ -10,14 +10,11 @@ func TestVPC(t *testing.T) {
 	t.Parallel()
 
 	terraformOptions := &terraform.Options{
-		// The path to where your Terraform code is located
 		TerraformDir: "../examples/complete-example",
 		Upgrade:      true,
 	}
 
-	// At the end of the test, run `terraform destroy` to clean up any resources that were created
 	defer terraform.Destroy(t, terraformOptions)
 
-	// This will run `terraform init` and `terraform apply` and fail the test if there are any errors
-	terraform.InitAndApply(t, terraformOptions)
+	terraform.InitAndApplyAndIdempotent(t, terraformOptions)
 }
