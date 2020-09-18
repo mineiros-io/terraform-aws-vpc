d5a202cf54cd33deccc7f1d0bfcdabb27876b7aa
Marius Tolzmann <m@rius.berlin>
2020-09-18 17:38:17 +0200

    Add workaround for broken output diff detection

--- test/unit_minimal_test.go
+++ test/unit_minimal_test.go
@@ -4,6 +4,7 @@ import (
 	"testing"
 
 	"github.com/gruntwork-io/terratest/modules/terraform"
+	"github.com/stretchr/testify/assert"
 )
 
 func TestUnitMinimal(t *testing.T) {
@@ -20,5 +21,12 @@ func TestUnitMinimal(t *testing.T) {
 	defer terraform.Destroy(t, terraformOptions)
 
 	terraform.InitAndPlan(t, terraformOptions)
-	terraform.ApplyAndIdempotent(t, terraformOptions)
+	terraform.Apply(t, terraformOptions)
+
+	stdout := terraform.Plan(t, terraformOptions)
+
+	resourceCount := terraform.GetResourceCount(t, stdout)
+	assert.Equal(t, 0, resourceCount.Add, "No resources should have been created. Found %d instead.", resourceCount.Add)
+	assert.Equal(t, 0, resourceCount.Change, "No resources should have been changed. Found %d instead.", resourceCount.Change)
+	assert.Equal(t, 0, resourceCount.Destroy, "No resources should have been destroyed. Found %d instead.", resourceCount.Destroy)
 }
